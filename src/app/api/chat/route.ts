00b8f83c88808048e386cf011b7918dbfc94262d
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-02-26 23:56:27 +0530

    Type script errors fixed

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,28 +1,31 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel(
-  { 
-    model: "gemini-1.5-flash" }
-);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
-export async function POST(req: [NextRequest]) {
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+export async function POST(req: NextRequest) {
   try {
     const { history = [], chat } = await req.json();
-    
+
     const chatSession = model.startChat({ history });
 
     const result = await chatSession.sendMessage(chat);
 
     const response = await result.response;
-    
+
     const text = response.text();
 
-    return Response.json({ text });
+    return NextResponse.json({ text });
+  } catch (error: any) {
+    console.error(error);
 
-  } catch (error) {
-    console.log(error);
-    return Response.json({ error: error.message }, { status: 500 });
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      { status: 500 }
+    );
   }
 }
