001ec559532ce27ae5d0f2b43226fa7c28c0a211
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-02-28 09:37:21 +0530

    Bugs were addressed

--- src/components/loginButton.tsx
+++ src/components/loginButton.tsx
@@ -1,22 +1,10 @@
 "use client";
 
-import loginAction from "@/app/actions/loginAction";
-import { useSession, signIn, signOut } from "next-auth/react";
-
 function LoginButton() {
-    async function handleLogin() {
-        try {
-            const user = await loginAction();
-            console.log("User logged in:", user);
-        } catch (error) {
-            console.error("Login failed:", error);
-        }
-    }
-
     return (
         <button
             className="border-2 py-2 px-6 mx-5 bg-zinc-800 rounded-lg text-white"
-            onClick={() => {signIn("google")}}
+            onClick={() => {alert("To Be implemented")}}
         >
             Log In
         </button>
