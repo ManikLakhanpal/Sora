6e200bf747b5a0055f7734b662b48dda6e5a4b2a
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-03-01 22:59:34 +0530

    Schema created and integrated

--- src/app/lib/dbConnect.ts
+++ src/app/lib/dbConnect.ts
@@ -28,7 +28,7 @@ async function dbConnect(): Promise<void> {
     }
 
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI!);
+        const db = await mongoose.connect(`${process.env.MONGODB_URI!}/sora`);
         connection.isConnected = db.connections[0].readyState;
 
         console.log("DB connected successfully");
